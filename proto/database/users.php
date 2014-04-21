b51e853ecbb96615244bcd607e3e8b328062be04
AnaSousa <ei11068@fe.up.pt>
2014-04-21 17:19:14 +0100

    added some functions

--- proto/database/users.php
+++ proto/database/users.php
@@ -86,5 +86,90 @@ function updateUserProfile($idUser, $imageLink, $about, $birthDate, $city, $emai
     $stmt->execute();
 }
 
+//alterar password
+function changePassword($idUser, $password)
+{
+    global $conn;
+
+    $stmt = $conn->prepare("UPDATE webUser
+        SET password = :password WHERE idUser = :idUser;");
+
+    $stmt->bindParam(":password", sha1($password));
+    $stmt->bindParam(":idUser", $idUser);
+
+    $stmt->execute();
+}
+
+function getUsersByName() {
+
+    global $conn;
+    $stmt = $conn->prepare("SELECT  username, imageLink,  registrationDate,
+        about,  birthDate,  city,  email,  gender,  name,  numAnswers,  
+        numComments,  numQuestions,  score,  idCountry,  userGroup
+        FROM webUser
+        ORDER BY username");
+    
+    $stmt->execute();
+    $stmt->fetch();
+
+    $data = array();
+
+    if ($stmt->num_rows() > 0) {
+        foreach ($stmt->result() as $row) {
+            $data[$row->id] = $row->name;
+        }
+    }
+
+    return json_encode($data);
+}
+
+function getUsersByScore() {
+
+    global $conn;
+    $stmt = $conn->prepare("SELECT  username, imageLink,  registrationDate,
+        about,  birthDate,  city,  email,  gender,  name,  numAnswers,  
+        numComments,  numQuestions,  score,  idCountry,  userGroup
+        FROM webUser
+        ORDER BY score");
+    
+    $stmt->execute();
+    $stmt->fetch();
+
+    $data = array();
+
+    if ($stmt->num_rows() > 0) {
+        foreach ($stmt->result() as $row) {
+            $data[$row->id] = $row->name;
+        }
+    }
+
+    return json_encode($data);
+}
+
+//TODO: SQL008 - Obter valores de estatísticas
+
+function promoteUser($idUser) {
+
+    global $conn;
+
+    $stmt = $conn->prepare("UPDATE webUser
+        SET userGroup = 'moderator' WHERE idUser = :idUser;");
+
+    $stmt->bindParam(":idUser", $idUser);
+
+    $stmt->execute();
+}
+
+function deleteUser($idUser) {
+    
+    global $conn;
+
+    $stmt = $conn->prepare("DELETE FROM webUser
+        WHERE idUser = :idUser;");
+
+    $stmt->bindParam(":idUser", $idUser);
+
+    $stmt->execute();
+}
 
 ?>
