b0849d0282dbc53277a4fa62bc7c37c1392c22cd
AnaSousa <ei11068@fe.up.pt>
2014-04-24 15:25:31 +0100

    uncommented

--- proto/pages/users/profile.php
+++ proto/pages/users/profile.php
@@ -3,12 +3,12 @@
 include_once('../../config/init.php');
 include_once($BASE_DIR .'database/users.php'); //dados pessoais
 include_once($BASE_DIR .'database/questions.php'); //perguntas pessoais
-/*
+
 if (!$_GET['username']) {
     $_SESSION['error_messages'][] = 'Undefined username';
     header("Location: $BASE_URL");
     exit;
-}*/
+}
 
 $idUser=getIdUser(amet);//$_GET['username']);
 $profile_data=getUserProfile($idUser);
