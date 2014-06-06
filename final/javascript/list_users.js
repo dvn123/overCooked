f336238226e7631ce87699a181dd475a8b417b37
Augusto Silva <ei11113@fe.up.pt>
2014-06-06 09:58:58 +0100

    validate!

--- final/javascript/list_users.js
+++ final/javascript/list_users.js
@@ -11,8 +11,8 @@ function searchIt()
 	var user = $("#search").val();
     if(user != "")
     {
-       $(".myuser[user^="+user+"]").show();
-   	   $(".myuser:not([user^="+user+"])").hide();	
+       $(".myuser[id^="+user+"]").show();
+   	   $(".myuser:not([id^="+user+"])").hide();	
     } else
     {
     	$(".myuser").show();
