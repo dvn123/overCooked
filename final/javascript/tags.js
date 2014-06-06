3d6f8ed00450b7921d28b9169261600233fba009
Augusto Silva <ei11113@fe.up.pt>
2014-06-06 09:47:47 +0100

    tag attr name

--- final/javascript/tags.js
+++ final/javascript/tags.js
@@ -11,8 +11,8 @@ function searchIt()
 	var tag = $("#search").val();
     if(tag != "")
     {
-       $(".mytag[tag^="+tag+"]").show();
-   	   $(".mytag:not([tag^="+tag+"])").hide();	
+       $(".mytag[tagname^="+tag+"]").show();
+   	   $(".mytag:not([tagname^="+tag+"])").hide();	
     } else
     {
     	$(".mytag").show();
